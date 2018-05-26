9683846e73c7f3e9dc930a7b83a7e6645b31944e
BenajminCHEN <BenajminCHEN2016@outlook.com>
2018-05-26 17:27:46 +0800

    fuck jar

--- src/main/java/com/compiler/principle/lab/controller/LexController.java
+++ src/main/java/com/compiler/principle/lab/controller/LexController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
 
 @RestController
@@ -23,7 +22,7 @@ public class LexController {
         try {
             return LexicalAnalyzer.analyze(payload.get("code"));
         } catch (Exception e){
-            return new LexResponse("sys error", null, null);
+            return new LexResponse("sys error", null, e.getMessage());
         }
 
     }
